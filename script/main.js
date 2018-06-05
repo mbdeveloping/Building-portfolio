523158032868d436977287da9495071f8257e659
Mantvydas <31690342+mbdeveloping@users.noreply.github.com>
2018-06-05 23:10:20 +0100

    update
    *shortening navigation button animation codel

--- script/main.js
+++ script/main.js
@@ -15,26 +15,17 @@ $(document).ready(function() {
       if (!navBtn.hasClass('navigation-open')) {
         navBtn.addClass('navigation-open');
         TweenMax.to(leftStickBot, .3, {rotation: -45, onComplete:function() {
-          TweenMax.to(leftStickBot,.3, {x: -5});
-          TweenMax.to(leftStickTop, .3, {x: -5});
-          TweenMax.to(rightStickTop, .3, {x: 5});
-          TweenMax.to(rightStickBot, .3, {x: 5});
+          TweenMax.to([leftStickBot, leftStickTop],.3, {x: -5});
+          TweenMax.to([rightStickTop, rightStickBot], .3, {x: 5});
           TweenMax.to(stickMiddle, .3, {height: '140%'});
         }});
-        TweenMax.to(leftStickTop, .3, {rotation: 45});
+        TweenMax.to([leftStickTop, rightStickBot], .3, {rotation: 45});
         TweenMax.to(rightStickTop, .3, {rotation: -45});
-        TweenMax.to(rightStickBot, .3, {rotation: 45});
       } else {
         navBtn.removeClass('navigation-open');
         TweenMax.to(stickMiddle, .3, {height: '0%'});
-        TweenMax.to(leftStickBot,.3, {x: 0});
-        TweenMax.to(leftStickTop, .3, {x: 0});
-        TweenMax.to(rightStickTop, .3, {x: 0});
-        TweenMax.to(rightStickBot, .3, {x: 0, onComplete:function() {
-          TweenMax.to(leftStickBot, .3, {rotation: 0});
-          TweenMax.to(leftStickTop, .3, {rotation: 0});
-          TweenMax.to(rightStickTop, .3, {rotation: 0});
-          TweenMax.to(rightStickBot, .3, {rotation: 0});
+        TweenMax.to([rightStickBot, leftStickBot, leftStickTop, rightStickTop], .3, {x: 0, onComplete:function() {
+          TweenMax.to([leftStickBot, leftStickTop, rightStickTop, rightStickBot], .3, {rotation: 0});
         }});
       }
     }
