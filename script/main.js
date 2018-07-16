a15b9b7c541cdf7de975b4730badec97825ca069
Mantvydas <31690342+mbdeveloping@users.noreply.github.com>
2018-07-16 23:28:15 +0100

    Update
    *hide earht from works page;

--- script/main.js
+++ script/main.js
@@ -50,6 +50,7 @@ document.addEventListener("DOMContentLoaded", function() {
         if (document.getElementById('home-header')) {
           TweenMax.staggerFromTo( '.testt span', 1, {autoAlpha:0, scale:1}, {autoAlpha:1, scale:1}, 0.05 );
           TweenMax.staggerFromTo( '.testt span', 0.1, {scale:4}, {scale:1}, 0.05, bigBenAndBus);
+          TweenMax.to('#earth',1, {y: '50%'});
           function bigBenAndBus() {
             TweenMax.fromTo('#big-ben', 1, {opacity:0, rotation:-45}, {opacity:1, rotation:0,ease: Elastic.easeOut.config(1, 0.3)});
             TweenMax.fromTo('#london-bus', 1, {opacity:0, x:-100}, {opacity:1, x:0,ease: Power4.easeOut});
@@ -59,6 +60,7 @@ document.addEventListener("DOMContentLoaded", function() {
           TweenMax.set('#about-header', {autoAlpha:1});
           TweenMax.from('#about-header h1', 1, {x:'-100%'});
           TweenMax.from('#about-header p', 1, {x:'100%'});
+          TweenMax.to('#earth',1, {y: '50%'});
         }
         if (document.getElementById('works-page-main-header')) {
           TweenMax.set('#works-page-main-header', {autoAlpha:1});
@@ -67,6 +69,7 @@ document.addEventListener("DOMContentLoaded", function() {
           TweenMax.from('#works-page-main-header h1', 1, {x:'-100%'});
           TweenMax.from('#works-page-main-header p', 1, {x:'100%'});
           TweenMax.from('#works-thumbnails', 1, {y:'100%', autoAlpha:0});
+          TweenMax.to('#earth',.3, {y: '100%'});
         }
       }
       //Leave home header animation
@@ -242,9 +245,7 @@ document.addEventListener("DOMContentLoaded", function() {
             navBtnClose();
             TweenMax.to(socialLinks, .3, {y:'100%'});
             TweenMax.to([leftNavOverlay, rightNavOverlay],.3, {width: '0%'});
-            // if (document.getElemenstByClassName('barba-container').namespace === 'works') {
-            //   console.log("to works")
-            // }
+            // TweenMax.to('#earth',1, {y: '100%'});
             TweenMax.to(homeLinks, .3, {color: 'rgba(255, 255, 255, 0)', onComplete:function() {
               TweenMax.to(['#hello', '#frontend-developer'], .7, {x:350});
               TweenMax.to(['#i-am-mantvydas', '#based'], .7, {x:-350, onComplete: allDone});
