5b66fa6fa16ab79a6760ed9331e44e164d468eb0
Mantvydas <31690342+mbdeveloping@users.noreply.github.com>
2018-08-11 14:07:42 +0100

    Update
    *remove old code

--- script/main.js
+++ script/main.js
@@ -72,13 +72,8 @@ document.addEventListener("DOMContentLoaded", function() {
       //   positionEarth();
       // });
 
-
-      // TweenMax.to('.twin', 3, {y:-100,x:-100, z:0, repeat:-1, ease:Linear.easeNone﻿});
       // TweenMax.to('#earth', 100, {rotation:360,z:0, repeat:-1, ease:Linear.easeNone﻿});
 
-      // TweenMax.to('#stars-small', 150, {y:-2000, z:0, repeat:-1, ease:Linear.easeNone﻿});
-      // TweenMax.to('#stars-medium', 200, {y:-2000, z:0, repeat:-1, ease:Linear.easeNone﻿});
-      // TweenMax.to('#stars-large', 250, {y:-2000, z:0, repeat:-1, ease:Linear.easeNone﻿});
       //Add class for all header spans
       function addClassForAllSpans() {
         document.querySelectorAll('#home-header-word-line-wrapper span').forEach(function(span) {
