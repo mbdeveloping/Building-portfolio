95019bc9e32b2f4343c4b6248efceb462285daa5
Mantvydas <31690342+mbdeveloping@users.noreply.github.com>
2018-08-30 22:11:55 +0100

    Update

--- script/main.js
+++ script/main.js
@@ -45,18 +45,18 @@ document.addEventListener("DOMContentLoaded", function() {
       }
       //Hide Works, About, social links
       function hideLinks() {
-        socialLinks.classList.remove('show-nav-social-links');
-        socialLinks.classList.add('hide-nav-social-links');
-        // TweenMax.to(socialLinks, .3, {z:0, y:'100%'});
+        // socialLinks.classList.remove('show-nav-social-links');
+        // socialLinks.classList.add('hide-nav-social-links');
+        TweenMax.to(socialLinks, .3, {z:0, y:'100%'});
         TweenMax.to(leftNavOverlay,.3, {z:0, x: '-100%'});
         TweenMax.to( rightNavOverlay,.3, {z:0, x: '100%'});
         TweenMax.to(homeLinks, .3, {color: 'rgba(255, 255, 255, 0)'});
       }
       function slieInLinks() {
         TweenMax.to([leftNavOverlay, rightNavOverlay], .5, {width: '5%', x:'0%'});
-        // TweenMax.to(socialLinks, .5, {y:'0%'});
-        socialLinks.classList.remove('hide-nav-social-links');
-        socialLinks.classList.add('show-nav-social-links');
+        TweenMax.to(socialLinks, .5, {y:'0%'});
+        // socialLinks.classList.remove('hide-nav-social-links');
+        // socialLinks.classList.add('show-nav-social-links');
         TweenMax.to(homeLinks, .3, {color: 'rgba(255, 255, 255, 1)'});
       }
 
